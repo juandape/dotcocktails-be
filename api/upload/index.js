f6dd1c86018b52491ae6ba32bcd19033227d0a2e
Juan David Peña <juandavidp76@hotmail.com>
2024-02-09 11:00:57 -0500

    change: upload routes

--- api/upload/index.js
+++ api/upload/index.js
@@ -7,6 +7,6 @@ const { isAuthenticated } = require('../../auth/auth.controller');
 const router = Router();
 const upload = multer({ dest: './temp' });
 
-router.post('/files', isAuthenticated, upload.array('files'), uploadHandler);
+router.post('/files', upload.array('files'), uploadHandler);
 
 module.exports = router;
